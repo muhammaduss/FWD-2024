54b72bb501085eb14168fd8b7e76544d62a082ce
muhammaduss <muhammad20040130@gmail.com>
2024-07-07 12:16:33 +0300

    adding gh pages path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import { base } from '$app/paths';
 	import Contacts from '$lib/Contacts.svelte';
 
 	let reachInfo: string = `You can reach me in Telegram from 10:00 to 23:00 any day of the week`;
@@ -10,8 +11,8 @@
 
 <header>
 	<nav>
-		<a href="/">Home</a>
-		<a href="/projects">Projects</a>
+		<a href="{base}/">Home</a>
+		<a href="{base}/projects">Projects</a>
 	</nav>
 </header>
 
--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: '/FWD-2024'
 		}
 	}
 };
